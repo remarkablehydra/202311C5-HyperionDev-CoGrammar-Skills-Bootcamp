438233381e77ad0a28f8b93847b7ec28d5039e66
KPramanik <140903750+remarkablehydra@users.noreply.github.com>
2024-01-19 01:28:48 +0000

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -16,4 +16,4 @@ print(nv1-nv2) # Displays difference
 
 print(nv3 * nv1) # Displays multiplication
 
-print((nv1+nv2+nv3) /nv3) #division by 3rd input number
+print((nv1+nv2+nv3) /nv3) # division by 3rd input number
