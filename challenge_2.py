8217d876cb8243acce74b687cbded3ff7a676db6
KPramanik <140903750+remarkablehydra@users.noreply.github.com>
2024-01-19 01:27:32 +0000

    Update challenge_2.py

--- challenge_2.py
+++ challenge_2.py
@@ -1,13 +1,13 @@
 
-#Write Python code to take the name of a user's favourite restaurant and store it in a variable called string_fav.
+# Write Python code to take the name of a user's favourite restaurant and store it in a variable called string_fav.
 string_fav = input('Please enter your favourite restaurant: ')
 
-#Write a statement to take in the user's favourite number. Use casting to store it in an integer variable called int_fav.
+# Write a statement to take in the user's favourite number. Use casting to store it in an integer variable called int_fav.
 int_fav = int(input('Please enter your favourite number: '))
 
-#Print out both of these using two separate print statements below what you have written.
+# Print out both of these using two separate print statements below what you have written.
 print('Your favourite restaurant is: ', string_fav)
 print('Your favourite number is: ', int_fav)
 
-#Try to cast string_fav to an integer. What happens?
-'''int(string_fav)''' #This will raise a ValueError because Python cannot convert a string that does not represent a number into an integer.
+# Try to cast string_fav to an integer. What happens?
+'''int(string_fav)''' # This will raise a ValueError because Python cannot convert a string that does not represent a number into an integer.
