54dd03ef9547aa346b2bcb255ccf632304ec6712
KPramanik <140903750+remarkablehydra@users.noreply.github.com>
2024-04-17 23:36:19 +0100

    Change const number #6

--- details sub 2.py
+++ details sub 2.py
@@ -6,8 +6,8 @@ use print command as output for sentence with all input commands'''
 
 name = input('Please enter your name: ')
 age = input('Please enter your age: ')
-number = input('Please enter your house number: ')
+housenumber = input('Please enter your house number: ')
 streetname = input('Please enter your street name: ')
 roadtype = input('Please confirm the type of road you live on: ')
-sentence = (f'This is {name}.They are {age} years old. They live at {number} {streetname} {roadtype}.')
+sentence = (f'This is {name}.They are {age} years old. They live at {housenumber} {streetname} {roadtype}.')
 print(sentence)
